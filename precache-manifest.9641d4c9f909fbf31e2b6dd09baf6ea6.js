551b0c91579a7859a9b86edc65aab7d78afa753d
Halil <halilpasho1@gmail.com>
2020-05-19 23:58:38 +0200

    Updates

---
+++ precache-manifest.9641d4c9f909fbf31e2b6dd09baf6ea6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b7800e0c5d5b1b3eb109e41710ec920",
+    "url": "/HalilPasho/React-card-list-project/index.html"
+  },
+  {
+    "revision": "fc39f4f308350d931b30",
+    "url": "/HalilPasho/React-card-list-project/static/css/main.bd6f2b70.chunk.css"
+  },
+  {
+    "revision": "692860676f74ec0d7591",
+    "url": "/HalilPasho/React-card-list-project/static/js/2.32e090c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HalilPasho/React-card-list-project/static/js/2.32e090c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc39f4f308350d931b30",
+    "url": "/HalilPasho/React-card-list-project/static/js/main.c1c37293.chunk.js"
+  },
+  {
+    "revision": "80f067fdb75b83e58100",
+    "url": "/HalilPasho/React-card-list-project/static/js/runtime-main.2746c1c8.js"
+  },
+  {
+    "revision": "fed8d09b3dff30b8453755ab1fc9ca4c",
+    "url": "/HalilPasho/React-card-list-project/static/media/greece.fed8d09b.jpg"
+  }
+]);
